cda615d62a5c1b33db31da702df1934085d135ef
Callum Farrell <20104331@mail.wit.ie>
2023-09-05 21:31:27 +0100

    Added Latest Reading util file

--- utils/latestreadings.js
+++ utils/latestreadings.js
@@ -1,2 +1,47 @@
 import { readingStore } from "../models/reading-store.js";
-import {conversions } from "../utils/conversions.js";
+import { conversions } from "../utils/conversions.js";
+
+export const latestReadings= async (id) => {
+    let stationReadings = await readingStore.getReadingsByStationId(id);
+    let latestReading = null;
+    const reading = {
+        latestCode: null,
+        latestCodeIcon: null,
+        latestTemp: null,
+        latestTempFahrenheit: null,
+        minTemp: null,
+        maxTemp: null,
+        latestWindSpeed: null,
+        latestWindSpeedBft: null,
+        minWindSpeed: null,
+        maxWindSpeed: null,
+        latestWindDirection: null,
+        windChill: null,
+        latestPressure: null,
+        minPressure: null,
+        maxPressure: null,
+    };
+
+    if (stationReadings.length > 0) {
+        latestReading = stationReadings.length - 1;
+        reading.latestCode = stationReadings[latestReading].code;
+        reading.latestCodeIcon = conversions.codeIcon(reading.latestCode);
+        reading.latestTemp = stationReadings[latestReading].temp;
+        reading.latestTempFahrenheit = conversions.celsiusToFahrenheit(reading.latestTemp);
+        reading.minTemp = conversions.calculateMin(reading.latestTemp);
+        reading.maxTemp = conversions.calculateMax(reading.latestTemp);
+        reading.latestWindSpeed = stationReadings[latestReading].windSpeed;
+        reading.latestWindSpeedBft = conversions.windSpeedToBeaufort(reading.latestWindSpeedBft);
+        reading.minWindSpeed = conversions.calculateMin(reading.latestWindSpeed);
+        reading.maxWindSpeed = conversions.calculateMax(reading.latestWindSpeed);
+        reading.latestWindDirection = stationReadings[latestReading].windDirection;
+        reading.windChill = conversions.windChill(reading.latestWindSpeed, reading.latestTemp);
+        reading.latestPressure = stationReadings[latestreading].pressure;
+        reading.minPressure = conversions.calculateMin(reading.latestPressure);
+        reading.maxPressure = conversions.calculateMax(reading.latestPressure);
+    }
+    return {
+        latestReading: latestReading,
+        reading: reading,
+    };
+};
