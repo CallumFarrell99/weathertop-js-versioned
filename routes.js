5426530084abaf09907e7fda07f57b07647f2ff5
Callum Farrell <20104331@mail.wit.ie>
2023-09-07 19:24:08 +0100

    Updated routes for new accounts methods

--- routes.js
+++ routes.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { accountsController } from "../controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import { stationController } from "./controllers/station-controller.js";
 
 export const router = express.Router();
+
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
 
 router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
